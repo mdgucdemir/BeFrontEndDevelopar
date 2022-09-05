a4038641a26b0baa6d3450ce048ababbbb8a80cc
mdgucdemir <99511022+mdgucdemir@users.noreply.github.com>
2022-09-06 02:06:53 +0300

    daily js

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/7-)Js Kosullar Donguler Fonksiyonlar/2-)Switch case/app.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/7-)Js Kosullar Donguler Fonksiyonlar/2-)Switch case/app.js
@@ -1,16 +1,40 @@
+/* Switch Case */
 
+// [switch case] ler [if] [else] lerin yerine kullanabilecegimiz bir yapi
 
-const process = 2;
+// const process = 2;
 
-if (process === 1){
-    console.log("process 1");
+// if (process === 1){
+//     console.log("process 1");
+// }
+// else if (process === 2){
+//     console.log("process 2");
+// }
+// else if (process === 3){
+//     console.log("process 3");
+// }
+// else {
+//     console.log(" no process");
+// }
+
+const process = 3;
+
+switch (process) {
+    case 1:
+        console.log("process 1");
+        break;
+    case 2:
+        console.log("process 2");
+        break;
+    case 3:
+        console.log("process 3");
+        break;
+    default:
+        console.log("no process");
+        break
+
 }
-else if (process === 2){
-    console.log("process 2");
-}
-else if (process === 3){
-    console.log("process 3");
-}
-else {
-    console.log(" no process");
-}
+
+// not = eger [case] lerin bitiminde [break] i yazman isen altaki case le birlikte yazar
+// ornegin case 1 ve case 2 yi birlikte calistirir
+// [case] in isi bince muhakkak [break] i yazmalisin
---
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/7-)Js Kosullar Donguler Fonksiyonlar/3-)Fonksiyonlar IIFE ve Anonim Fonksiyonlar/app.js
@@ -0,0 +1,126 @@
+// belir bir islem yapacak kodumuz var ise bunu bir [fonction] catisi altinda yazariz 
+// ihtiyacimiz oldukca o function i cagiririz
+// boylece durmadan ayni kodu durmadan yazma yada copy-paste yaparak [spacetti code] durumundan kurtaririz.
+
+// function tanimlama
+
+// function hello(){ 
+//     // su anda ekranda herhangi birsey yazmiyor biz sadece function tanimlama yaptik
+//     console.log("hello world");
+// }
+
+// funcion i csgirmak istiypr isek
+
+// hello(); // this is function call
+
+// function i istedigim kadar kullanabilirim
+
+// hello();
+// hello();
+// hello();
+// hello();
+
+// function hello (name, age){
+//     console.log(`your name: ${name} \nyour age: ${age}`);
+// }
+
+// biz function nin icine parametre gondere biliriz
+
+// hello("m. deniz",32);
+// hello("edward",32);
+
+// icine parametre gondermedigimiz icin [undefined] oldu
+// hello();
+
+
+// function mello (name, age){
+//     if(typeof name === "undefined") name = "no name";
+
+//     if(typeof age === "undefined") age = "no age";
+ 
+//     console.log(`your name: ${name} \nyour age: ${age}`);
+// }
+// mello();
+
+// // kisa yol direk default olarak atamak
+// function cello (name = "no name", age = "no age"){
+    
+//     console.log(`your name: ${name} \nyour age: ${age}`);
+// }
+
+// cello();
+// cello("joe");
+
+
+// fonksiyonlar da [return]
+// fonsiyonlarin cikisinda deger cikmiyor ve biz o degeri baska bir fonksiyonda kullanmak istedigimiz de NaN ciktisi oliyoruz
+// fonksiyonlarda [deger] donmesini istiyor isek, ki bu fonksiyondan deger ciktisi almaktir, [return] kulanicaz 
+// [return] bir fonksiyonu sonlandirir. return nun altina yazdiklarimiz calismanz
+
+// function square (x){
+//     return x * x;
+// }
+// function cube(x){
+//     return x * x * x;
+//     console.log("what's up"); // hic calistirilmadi
+// }
+
+
+// // let a = square(12);
+
+// // a = cube(a);
+
+// let a = cube(square(12));
+
+// console.log(a);
+
+// function hello(){
+//     return "hello world"
+// }
+// console.log(hello());
+
+
+
+// function Expression
+
+// biz burda bir fonksiyonumuzu bir degere esitledik
+// const hello = function(name){
+//     console.log("hello world" +" "+ name);
+// };
+
+// hello("m. deniz");
+
+
+
+// Immediatly Invoked Function Expression (IIFE)
+// turkce meali [tanimlandigi yerde calisan fonksiyonlar]
+
+// (function(name){
+//     console.log("hello world " + name );
+// })("edward");
+
+// bir objenin icinde function far ise biz buna [metod] diyoruz
+// objenin disinda var ise function diyoruz.
+
+
+const database = {
+    host: "localhost",
+    add: function(){
+        console.log("added");
+    },
+    get: function(){
+        console.log("get is done :)");
+    },
+    update: function(id){
+        console.log(`id: ${id} updated`);
+
+    },
+    delete: function(id){
+        console.log(`id: ${id} deleted`);
+
+    }
+}
+console.log(database.host);
+database.add();
+database.delete(10);
+database.update("edward");
