d77fc10ab1342d41213fe16d8b85201262619fcc
mdgucdemir <99511022+mdgucdemir@users.noreply.github.com>
2022-09-24 19:40:39 +0300

    daily js

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/11-)Project Todo Lists/todo.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/11-)Project Todo Lists/todo.js
@@ -1,12 +1,13 @@
 // Todo project
 
-// Tum elementleri secme
+
+// Element secimi
 
 const form = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo");
 const todoList = document.querySelector(".list-group");
-const firstCardBody = document.querySelector(".card-body")[0];
-const secondCardBody = document.querySelector(".card-body")[1];
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
 const filter = document.querySelector("#filter");
 const clearButton = document.querySelector("#clear-todos");
 
@@ -16,20 +17,77 @@ const clearButton = document.querySelector("#clear-todos");
 eventListeners();
 
 function eventListeners(){
-    form.addEventListener("submit",addTodo);
+  form.addEventListener("submit",addTodo);
+  document.addEventListener("DOMContentLoaded",loadAllTodosToUI);
 }
 
+function loadAllTodosToUI(){
+
+  let todos = getTodosFromStorage();
+
+  todos.forEach(function(todo){
+    // todo lar uzerinde tek tek gezinme
+    addTodoToUI(todo); // local storage da ki todo lari UI a cekme
+  })
+}
 function addTodo(e){
     const newTodo = todoInput.value.trim();
     // [.trim()] fonction i stringlerin basindaki ve sonundaki gereksiz bosluklari onlemek icindir
 
-    // yeni todo lari UI(user interface) e yazdirmak icin bir fonction cagirma
+    if (newTodo === ""){
+      /*
+      <div class="alert alert-danger" role="alert">
+        This is a danger alert—check it out!
+      </div>
+      */
+
+      // showAlert("type","message");
+
+      showAlert("danger","Please write a Todo");
+    }
+    else{
+
+       // yeni todo lari UI(user interface) e yazdirmak icin bir fonction cagirma
     addTodoToUI(newTodo);
-
-
-
-    // sayfayi default olarak yenilememesi icin [e.preventDefault();]
+    addTodoToStorage(newTodo);
+    showAlert("success","You're welcome")
+    }
+       // sayfayi default olarak yenilememesi icin [e.preventDefault();]
     e.preventDefault();
+}
+function getTodosFromStorage(){ // todo lari local storage dan alma
+  let todos;
+
+  if (localStorage.getItem("todos") === null){
+    todos = [];
+  }
+  else{
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  return todos;
+}
+
+
+function addTodoToStorage(newTodo){
+  let todos = getTodosFromStorage();
+
+  todos.push(newTodo); // yeni degeri todo ya ekleme
+  
+  localStorage.setItem("todos",JSON.stringify(todos)); // aldigimiz todo yu tekrar dan bunun icine gonderiyoruz
+}
+
+function showAlert(type,message){
+  const alert = document.createElement("div");
+
+  alert.className = `alert alert-${type}`;
+  alert.textContent = message;
+
+  firstCardBody.appendChild(alert);
+
+  // setTimeout
+  window.setTimeout(function(){
+    alert.remove();
+  },1000);
 }
 
 function addTodoToUI(newTodo){
