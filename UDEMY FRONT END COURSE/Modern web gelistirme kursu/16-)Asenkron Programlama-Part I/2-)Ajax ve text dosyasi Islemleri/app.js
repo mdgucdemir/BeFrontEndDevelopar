a443b194e6e488fd5357758094f66074a536f9de
mdgucdemir <99511022+mdgucdemir@users.noreply.github.com>
2022-11-02 23:59:08 +0300

    daily js

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/16-)Asenkron Programlama-Part I/2-)Ajax ve text dosyasi Islemleri/app.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/16-)Asenkron Programlama-Part I/2-)Ajax ve text dosyasi Islemleri/app.js
@@ -4,3 +4,16 @@
 // 403 = Forbidden(yasakli)
 // 404 = Not Found
 // 505 = Internal(ic,dahili) Server Error
+
+// ------------------------------------ 
+
+document.getElementById("btn").addEventListener("click",function(){
+
+    // Ajax islemimizi gerceklestirmeye calisalim
+    // XMLHttpRequest obejsinden bir tane olusturursak eger, Ajax islemimizi gerceklestiririz 
+
+    const xhr = new XMLHttpRequest();
+
+    // Obejemizi console a yazdiralim bakalim icerisinde neler var
+    console.log(xhr);
+});
