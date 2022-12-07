bb34fd6275dc55271e82fdc5b375161bb57faa7d
mdgucdemir <99511022+mdgucdemir@users.noreply.github.com>
2022-12-08 00:01:00 +0300

    daily js

---
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/21-)Github API Project/app.js
@@ -0,0 +1,33 @@
+// https://api.github.com/users/
+// https://api.github.com/users/mdgucdemir/
+
+// Elementleri secme
+
+const githubForm = document.getElementById("github-form"); // form alani
+const nameImput = document.getElementById("githubname"); // input alani
+const clearLastUsers = document.getElementById("clear-last-users"); // temizleme butonu
+const lastUsers = document.getElementById("last-users"); // <ul> alani 
+
+// Event Listeners
+eventListeners();
+
+function eventListeners(){
+
+    githubForm.addEventListener("submit",getData);
+    clearLastUsers.addEventListener("click",clearAllSearched);
+    document.addEventListener("DOMContentLoaded",getAllSearched); // son aramalari sayfa yenilendikce storage dan alip arayuzumuze yazacagiz
+}
+
+function getData(e){
+
+
+    e.preventDefault();
+}
+
+function clearAllSearched(){
+    // tum arananlari temizle
+}
+
+function getAllSearched(){
+    // arananlari storage dan al ve UI ya ekle
+}
---
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/21-)Github API Project/github.js
@@ -0,0 +1,9 @@
+class Github{
+    constructor(){
+        this.url = "https://api.github.com/users/";
+    }
+
+    async getGithubData(username){
+        const responseUser = await fetch(this.url + username);
+    }
+}
