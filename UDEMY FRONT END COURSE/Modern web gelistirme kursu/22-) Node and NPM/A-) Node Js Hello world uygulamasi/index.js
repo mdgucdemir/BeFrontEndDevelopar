fa5dca9c1699c6fee0a2918d0a65d79e99bc88e3
mdgucdemir <99511022+mdgucdemir@users.noreply.github.com>
2022-12-11 02:41:40 +0300

    daily js

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/22-) Node and NPM/A-) Node Js Hello world uygulamasi/index.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/22-) Node and NPM/A-) Node Js Hello world uygulamasi/index.js
@@ -2,8 +2,22 @@
 
 // console.log("mama miya");
 
-const a = 12;
-const b = 23;
+// const a = 12;
+// const b = 23;
 
-console.log("hello word");
-console.log(a+b);
+// console.log("hello word");
+// console.log(a+b);
+
+const a = 10;
+const b = -23;
+const c = a*b;
+
+if(c >= 0){
+    console.log(c," islem pozitif bir sayi")
+}
+else{
+    console.log(c," islem negatif bir sayi")
+}
+
+
+// terminal clear [cls]
