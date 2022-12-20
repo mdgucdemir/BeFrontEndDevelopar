ca814036f11269a9a0ca001bc8219c19dede7223
mdgucdemir <99511022+mdgucdemir@users.noreply.github.com>
2022-12-20 17:46:53 +0300

    daily js

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/24-)Moduller ile Calisan Projosi/src/index.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/24-)Moduller ile Calisan Projosi/src/index.js
@@ -1,4 +1,4 @@
 console.log("Test");
 console.log("Test-2");
-console.log("Testing Testing. Houston this is testing. Copy?");
+console.log("Testing Testing. Houston this is testing. Copy");
 console.log("fine");
