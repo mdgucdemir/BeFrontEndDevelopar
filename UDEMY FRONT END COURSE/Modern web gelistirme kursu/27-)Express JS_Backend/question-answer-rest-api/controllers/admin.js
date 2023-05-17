1554c6bce328cbea7e999a104a0f3268a897c6c8
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-05-17 22:52:21 +0300

    daily js

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/27-)Express JS_Backend/question-answer-rest-api/controllers/admin.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/27-)Express JS_Backend/question-answer-rest-api/controllers/admin.js
@@ -4,7 +4,7 @@ const CustomError = require("../helpers/error/CustomError");
 const asyncErrorWrapper = require("express-async-handler");
 
 
-const blockUser = asyncErrorWrapper(async(req,res,next) => {
+const blockUser = asyncErrorWrapper(async(req,res,next) => {   
     
     const {id} = req.params;   
     const user = await User.findById(id); 
