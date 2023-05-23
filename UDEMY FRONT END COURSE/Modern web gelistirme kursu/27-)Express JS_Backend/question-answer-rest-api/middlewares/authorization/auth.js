b3f353e944876198e21c69375e82e1245877e384
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-05-23 20:01:17 +0300

    daily js

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/27-)Express JS_Backend/question-answer-rest-api/middlewares/authorization/auth.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/27-)Express JS_Backend/question-answer-rest-api/middlewares/authorization/auth.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const asyncErrorWrapper = require("express-async-handler");
 const User = require("../../models/User1");
 const {isTokenIncluded, getAccessTokenFromHeader} = require("../../helpers/authorization/tokenHelpers");
+const Question = require("../../models/Question");
 
 
 const getAccessToRoute = (req,res,next) => {
@@ -50,7 +51,22 @@ const getAdminAccess = asyncErrorWrapper(async(req,res,next) => {
 
 }); 
 
+const getQuestionOwnerAccess = asyncErrorWrapper(async(req,res,next) => {
+    
+    const userId = req.user.id;
+    const questionId = req.params.id;
+
+    const question = await Question.findById(questionId);
+
+    if (question.user != userId) {
+        return next(new CustomError("Only owner can handle this operation",403));
+    }
+    next();
+});
+    
+
 module.exports = {
     getAccessToRoute,
-    getAdminAccess
+    getAdminAccess,
+    getQuestionOwnerAccess
 };
--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/27-)Express JS_Backend/question-answer-rest-api/routers/question.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/27-)Express JS_Backend/question-answer-rest-api/routers/question.js
@@ -3,16 +3,18 @@ const express = require('express');
 const {
     askNewQuestion,
     getAllQuestions,
-    getSingleQuestion
+    getSingleQuestion,
+    editQuestion
 } = require('../controllers/question');
 
 const {checkQuestionExist} = require("../middlewares/database/databaseErrorHelpers");
-const {getAccessToRoute} = require("../middlewares/authorization/auth");
+const {getAccessToRoute, getQuestionOwnerAccess} = require("../middlewares/authorization/auth");
 const router = express.Router();
 
 router.post("/ask",getAccessToRoute,askNewQuestion);
 router.get("/:id",checkQuestionExist,getSingleQuestion);
 router.get("/",getAllQuestions);
+router.put("/:id/edit",[getAccessToRoute,checkQuestionExist,getQuestionOwnerAccess],editQuestion);
 
 // router.get("/delete", (req,res) =>{
 //     res.send("Question Delete Page")
