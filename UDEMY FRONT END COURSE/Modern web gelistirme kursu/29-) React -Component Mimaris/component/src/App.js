aaae5e74f6b56034b7fe1f42c162410bff71ed27
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-06-08 16:45:43 +0300

    react component

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/29-) React -Component Mimaris/component/src/App.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/29-) React -Component Mimaris/component/src/App.js
@@ -1,9 +1,16 @@
-
+import CategoryList from './CategoryList';
+import Navi from './Navi';
+import ProductList from './ProductList';
 
 function App() {
   return (
     <div>
-      <h3>Hello react :D</h3>
+      {/* <h1>Hello react :D</h1> */}
+
+      {/* <Navi></Navi> bu yapiyi asagida sade bir bicim de yazilmis hali mevcuttur */}
+      <Navi/>
+      <CategoryList/>
+      <ProductList/>
     </div>
   );
 }
---
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/29-) React -Component Mimaris/component/src/ProductList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        <h3>This is product list</h3>
+      </div>
+    )
+  }
+}
