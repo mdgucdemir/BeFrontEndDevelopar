7ee6f07abc081ea63757276001c18625775e4ea4
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-06-09 19:12:59 +0300

    react state

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/29-) React -Component Mimaris/component/src/CategoryList.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/29-) React -Component Mimaris/component/src/CategoryList.js
@@ -2,28 +2,38 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+  // constructor(props) {
+  //     super(props);
+  //     this.state : {}
+  // }
 
-    // constructor(props) {
-    //     super(props);
-    //     state : {}
-    // }
+  // react props yontemi
+  // props = [properties] = probs ta bir companent den digerine bir degisken, data, bir event tasinir.
+  // react e yeni gelen guncellemeler ile birlik yukardaki gibi bir yazim olmadan da assagidaki {this.props.title} ornegini yapabiliriz
 
-    // react props yontemi
-    // props = [properties] = probs ta bir companent den digerine bir degisken bir event tasinir.
-    // react e yeni gelen guncellemeler ile birlik yukardaki gibi bir yazim olmadan da assagidaki {this.props.title} ornegini yapabiliriz
+  // props = bir companent ten digerine data tasima yontemidir
+  // state = bir companent in data sidir. Bir companent e ozel bir veri tutmak istiyorsaniz bunu state ile yapabilirsiniz
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" },
+      ],
+    };
+  }
 
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
-        <h3>{this.props.info.something}</h3>
         <ListGroup>
-          <ListGroupItem>Hello</ListGroupItem>
-          <ListGroupItem>React</ListGroupItem>
-          <ListGroupItem>This</ListGroupItem>
-          <ListGroupItem>is</ListGroupItem>
-          <ListGroupItem>Me</ListGroupItem>
-          <ListGroupItem>:)</ListGroupItem>
+          {this.state.categories.map((category) => (
+            <ListGroupItem key={category.categoryId}>
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
         </ListGroup>
       </div>
     );
