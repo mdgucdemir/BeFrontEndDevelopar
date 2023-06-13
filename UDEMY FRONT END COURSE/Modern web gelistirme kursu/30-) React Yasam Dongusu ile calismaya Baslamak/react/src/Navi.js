dd0e954f1bda2816031a48656dede725cec7ba5d
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-06-13 22:12:59 +0300

    daily react

---
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/30-) React Yasam Dongusu ile calismaya Baslamak/react/src/Navi.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Navi Component</h1>
+      </div>
+    )
+  }
+  
+  // bu yapiyi [rcc] yazip enter ladigimizda geliyor. React extension sayesinde
+  
+}
+
