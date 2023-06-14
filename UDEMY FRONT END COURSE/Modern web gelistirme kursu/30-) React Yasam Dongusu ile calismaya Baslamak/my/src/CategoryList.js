408c405ffed83b0e62fe8d0056d2d7466bce5e1b
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-06-14 19:08:19 +0300

    daily react

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/30-) React Yasam Dongusu ile calismaya Baslamak/my/src/CategoryList.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/30-) React Yasam Dongusu ile calismaya Baslamak/my/src/CategoryList.js
@@ -2,30 +2,7 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-  // constructor(props) {
-  //     super(props);
-  //     this.state : {}
-  // }
-
-  // react props yontemi
-  // props = [properties] = probs ta bir companent den digerine bir degisken, data, bir event tasinir.
-  // react e yeni gelen guncellemeler ile birlik yukardaki gibi bir yazim olmadan da assagidaki {this.props.title} ornegini yapabiliriz
-
-  // props = bir companent ten digerine data tasima yontemidir
-  // state = bir companent in data sidir. Bir companent e ozel bir veri tutmak istiyorsaniz bunu state ile yapabilirsiniz
-
-  // State Klasik yazim
-
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     categories: [
-  //       { categoryId: 1, categoryName: "Beverages" },
-  //       { categoryId: 2, categoryName: "Condiments" },
-  //     ],
-  //   };
-  // }
-
+  
   // State Sade yazim
 
   state = {
