1a72e35cf0ea0c753716357ed831d0683891f9a2
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-06-14 21:45:46 +0300

    componentDidMount and fetch

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/30-) React Yasam Dongusu ile calismaya Baslamak/my/src/CategoryList.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/30-) React Yasam Dongusu ile calismaya Baslamak/my/src/CategoryList.js
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-  
   // State Sade yazim
 
   state = {
-    categories: [
-      { categoryId: 1, categoryName: "Beverages" },
-      { categoryId: 2, categoryName: "Condiments" },
-    ],
-    
-  }; 
+    categories: [],
+  };
+
+  componentDidMount() {
+    this.getcategories();
+  }
+
+  getcategories = () => {
+    fetch("http://localhost:3000/categories")
+      .then((response) => response.json())
+      .then((data) => this.setState({ categories: data }));
+  };
 
   render() {
     return (
@@ -21,7 +26,7 @@ export default class CategoryList extends Component {
           {this.state.categories.map((category) => (
             <ListGroupItem
               onClick={() => this.props.changeCategory(category)}
-              key={category.categoryId}
+              key={category.id}
             >
               {category.categoryName}
             </ListGroupItem>
