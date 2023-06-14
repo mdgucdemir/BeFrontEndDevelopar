e40c2f06bc3c0c4caee309dcc9d218c59bc7f966
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-06-15 00:44:27 +0300

    react table

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/30-) React Yasam Dongusu ile calismaya Baslamak/my/src/App.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/30-) React Yasam Dongusu ile calismaya Baslamak/my/src/App.js
@@ -1,4 +1,3 @@
-
 import { Col, Container, Row } from "reactstrap";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
@@ -9,48 +8,68 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   // category deki datayi product a tasimak icin, category datasini ilk basta app{bir class olusturarak function degil} e sonra producta tasiyoruz.
-  // component ler arasi veri transferini ancak bu sekilde yapabiliyoruz ne yazik ki 
+  // component ler arasi veri transferini ancak bu sekilde yapabiliyoruz ne yazik ki
 
-  state = {currentCategory: ""}
+  state = { currentCategory: "", products: [] };
 
   changeCategory = (category) => {
     this.setState({ currentCategory: category.categoryName });
   };
 
-  render() {
+  componentDidMount() {
+    this.getProducts();
+  }
 
+  getProducts = () => {
+    fetch("http://localhost:3000/products")
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+
+  render() {
     //  props ile encapsulation (kapsulleme)
-    let categoryInfo = {title : "This is Category List", something : "An other Something"}; // sadece burada degisiklik yapmamiz yeterli
-    let productInfo = {title : "This is Product List"};
+    let categoryInfo = {
+      title: "This is Category List",
+      something: "An other Something",
+    }; // sadece burada degisiklik yapmamiz yeterli
+    let productInfo = { title: "This is Product List" };
     // yukaridaki gibi degisken atama yontemi ile de islemlerimiz gerceklestirilebilinir
 
-    return (           
-      
+    return (
       <div>
         {/* <h1>Hello react :D</h1> */},
-  
-        {/* <Navi></Navi> bu yapiyi asagida sade bir bicim de yazilmis hali mevcuttur */};
-        {/*[npm install bootsrap] ve [npm install reactstrap] yapmalisin. ikisinide yuklemelisin yoksa calismaz*/},
+        {/* <Navi></Navi> bu yapiyi asagida sade bir bicim de yazilmis hali mevcuttur */}
+        ;
+        {/*[npm install bootsrap] ve [npm install reactstrap] yapmalisin. ikisinide yuklemelisin yoksa calismaz*/}
+        ,
         <Container>
           <Row>
             <Navi />
           </Row>
           <Row>
             <Col xs="3">
-              <CategoryList currentCategory = {this.state.currentCategory} changeCategory = {this.changeCategory} info = {categoryInfo} />
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
             </Col>
             <Col xs="9">
-              <ProductList currentCategory = {this.state.currentCategory} info = {productInfo} />
+              <ProductList
+                products={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
             </Col>
           </Row>
         </Container>
-      </div>      
+      </div>
     );
   }
 }
 
 // function App() {
-  
+
 // }
 
 // export default App;
