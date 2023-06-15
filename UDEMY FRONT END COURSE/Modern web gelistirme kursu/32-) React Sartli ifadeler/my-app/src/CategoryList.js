f71321ac1155141b42604c8e34547c7b477386fb
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-06-15 23:45:42 +0300

    react conditions

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/32-) React Sartli ifadeler/my-app/src/CategoryList.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/32-) React Sartli ifadeler/my-app/src/CategoryList.js
@@ -24,7 +24,8 @@ export default class CategoryList extends Component {
         <h3>{this.props.info.title}</h3>
         <ListGroup>
           {this.state.categories.map((category) => (
-            <ListGroupItem
+            <ListGroupItem 
+            active = {category.categoryName === this.props.currentCategory ? true:false}
               onClick={() => this.props.changeCategory(category)}
               key={category.id}
             >
