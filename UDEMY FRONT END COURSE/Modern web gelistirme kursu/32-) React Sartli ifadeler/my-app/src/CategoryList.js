347a0285c5b0371886a2855d3f835439c6e40d77
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-06-15 23:34:22 +0300

    daily react

---
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/32-) React Sartli ifadeler/my-app/src/CategoryList.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+
+export default class CategoryList extends Component {
+  // State Sade yazim
+
+  state = {
+    categories: [],
+  };
+
+  componentDidMount() {
+    this.getcategories();
+  }
+
+  getcategories = () => {
+    fetch("http://localhost:3000/categories")
+      .then((response) => response.json())
+      .then((data) => this.setState({ categories: data }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>{this.props.info.title}</h3>
+        <ListGroup>
+          {this.state.categories.map((category) => (
+            <ListGroupItem
+              onClick={() => this.props.changeCategory(category)}
+              key={category.id}
+            >
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+
+          {/* <h4>{this.props.currentCategory}</h4> */}
+        </ListGroup>
+      </div>
+    );
+  }
+}
