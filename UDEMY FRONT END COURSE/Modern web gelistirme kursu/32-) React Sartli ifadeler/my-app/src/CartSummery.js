c5b2cd2e2e98d73f90959252f25546d4b6039243
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-06-17 21:28:24 +0300

    react component drilling

---
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/32-) React Sartli ifadeler/my-app/src/CartSummery.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import {
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Badge,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+export default class CartSummery extends Component {
+  renderSummary() {
+    return (
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>
+          Cart - {this.props.cart.length}
+        </DropdownToggle>
+        <DropdownMenu end>
+          {this.props.cart.map((cartItem) => (
+            <DropdownItem key={cartItem.product.id}>
+              {cartItem.product.productName}
+              <Badge color="success">{cartItem.quantity}</Badge>
+            </DropdownItem>
+          ))}
+          <DropdownItem divider />
+          <DropdownItem>Reset</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    );
+  }
+
+  renderEmptyCart () {
+    return (
+        <NavItem>
+            <NavLink>Empty Cart</NavLink>
+        </NavItem>
+    )
+  }
+  render() {
+    return (
+      <div>
+        {this.props.cart.length > 0 ? this.renderSummary() : this.renderEmptyCart()}
+      </div>
+    );
+  }
+}
--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/32-) React Sartli ifadeler/my-app/src/Navi.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/32-) React Sartli ifadeler/my-app/src/Navi.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Collapse,
   Navbar,
@@ -6,59 +6,54 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,  
-} from 'reactstrap';
+  NavLink  
+} from "reactstrap";
+import CartSummery from "./CartSummery";
+
+
 export default class Navi extends React.Component {
   constructor(props) {
     super(props);
 
     this.toggle = this.toggle.bind(this);
     this.state = {
-      isOpen: false
+      isOpen: false,
     };
   }
   toggle() {
     this.setState({
-      isOpen: !this.state.isOpen
-    });    
+      isOpen: !this.state.isOpen,
+    });
   }
 
-  render(){
+  render() {
     return (
       <div>
-        <Navbar color="light" light expand='md'>
+        <Navbar color="light" light expand="md">
           <NavbarBrand href="/">My App</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar className='justify-content-end'>
+          <Collapse
+            isOpen={this.state.isOpen}
+            navbar
+            className="justify-content-end"
+          >
             <Nav className="ml-auto" navbar>
               <NavItem>
                 <NavLink href="/components/">Components</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap" target='_blank'>
+                <NavLink
+                  href="https://github.com/reactstrap/reactstrap"
+                  target="_blank"
+                >
                   GitHub
                 </NavLink>
               </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Cart - {this.props.cart.length}
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>Option 1</DropdownItem>
-                  <DropdownItem>Option 2</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Reset</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>         
+              <CartSummery cart={this.props.cart}/>   
+            </Nav>
           </Collapse>
         </Navbar>
       </div>
     );
   }
 }
-
