1ad31411498406bd10320215136f233e70dbe462
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-06-27 01:03:44 +0300

    react form submit

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/34-) React Formlarla Calismak/my-app/src/FormDemo1.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/34-) React Formlarla Calismak/my-app/src/FormDemo1.js
@@ -8,13 +8,20 @@ export default class FormDemo1 extends Component {
     this.setState({userName:event.target.value})
   }
 
+  onSubmitHandler = (event) => {
+    event.preventDefault(); /* Default davranisi ugulama demek. bu da bize submit edildikten sonra cart taki elemanlarin kaybolmamasini sagliyor.
+     Cunku ugulamamiz single page uygulamadir */
+    alert(this.state.userName);
+  }
+
   render() {
     return (
       <div>
-        <Form>
+        <Form onSubmit={this.onSubmitHandler}>
             <h3>User Name</h3>
             <Input onChange={this.onChangeHandler} type="text"></Input>
             <h3>User Name is {this.state.userName}</h3>
+            <Input type="submit" value="save"></Input>
         </Form>
       </div>
     );
