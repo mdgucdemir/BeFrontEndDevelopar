e6e08c69cba7733a1aac7d1d20029d57a7f3e544
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-06-28 21:07:40 +0300

    react form

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/34-) React Formlarla Calismak/my-app/src/FormDemo1.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/34-) React Formlarla Calismak/my-app/src/FormDemo1.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import { Form, Input } from "reactstrap";
 
 export default class FormDemo1 extends Component {
-  state = { userName: "" };
+  state = { userName: "", city: "" };
 
   onChangeHandler = (event) => {
-    this.setState({userName:event.target.value})
+    //this.setState({userName:event.target.value})
+
+    let name = event.target.name;
+    let value = event.target.value;
+
+    this.setState({[name]:value})
   }
 
   onSubmitHandler = (event) => {
@@ -19,8 +24,13 @@ export default class FormDemo1 extends Component {
       <div>
         <Form onSubmit={this.onSubmitHandler}>
             <h3>User Name</h3>
-            <Input onChange={this.onChangeHandler} type="text"></Input>
+            <Input name="userName" onChange={this.onChangeHandler} type="text"></Input>
             <h3>User Name is {this.state.userName}</h3>
+
+            <h3>City</h3>
+            <Input name="city" onChange={this.onChangeHandler} type="text"></Input>
+            <h3>City is {this.state.city}</h3>
+
             <Input type="submit" value="save"></Input>
         </Form>
       </div>
