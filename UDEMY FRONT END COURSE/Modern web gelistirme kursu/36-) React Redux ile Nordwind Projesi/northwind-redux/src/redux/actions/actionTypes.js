d27e2888d3395579c91312ee87384fd9c73eb52a
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-07-03 22:37:20 +0300

    react redux

---
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/36-) React Redux ile Nordwind Projesi/northwind-redux/src/redux/actions/actionTypes.js
@@ -0,0 +1,4 @@
+// action isimlerini state lerimize uygun isimler ile vermeliyiz
+
+export const CHANGE_CATEGORY = "CHANGE_CATEGORY"
+export const GET_CATEGORIES_SUCCESS = "GET_CATEGORIES_SUCCESS" // category state inide redux tarafinda tutmak istedik
