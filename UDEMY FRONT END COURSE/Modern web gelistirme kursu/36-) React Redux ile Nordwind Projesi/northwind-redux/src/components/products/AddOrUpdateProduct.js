0938d454e87f6db091e8a3ce1914f8131d089cce
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-07-18 00:46:24 +0300

    react hook

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/36-) React Redux ile Nordwind Projesi/northwind-redux/src/components/products/AddOrUpdateProduct.js
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/36-) React Redux ile Nordwind Projesi/northwind-redux/src/components/products/AddOrUpdateProduct.js
@@ -1,48 +1,80 @@
-
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { getCategories } from "../../redux/actions/categoryActions";
-import { saveProduct } from "../../redux/actions/productActions"
+import { saveProduct } from "../../redux/actions/productActions";
 
 // useState ==> [ setState ] in yerine kullanicaz
 // useEffect ==> [ componentDidMount ] un yerine kullanicaz
 
 function AddorUpdateProduct({
-    products,
-    categories,
-    getProducts,
-    getCategories,
-    saveProduct,
-    history,
-    ...props
+  products,
+  categories,
+  getProducts,
+  getCategories,
+  saveProduct,
+  history,
+  ...props
 }) {
-    const [product,setProduct] = useState({...props.product})
-    // product state ini setProduct funtion ile set edebilirim demektir yukaridaki yazim. Bu yazima js de distracting deniliyor.
+  const [product, setProduct] = useState({ ...props.product });
+  // product state ini setProduct funtion ile set edebilirim demektir yukaridaki yazim. Bu yazima js de distracting deniliyor.
 
-    useEffect(()=>{
-        if(categories.length === 0){
-            getCategories();
-        }
-        setProduct({...props.product});
-        // useEffect life cycle da ki butun herseyi yonetmeye calisiyor. ve Sonsuz bir donguye giriyor
-        // sonsuz donguden kurtulmak icin " }.[sonsuz donguden kurtarmak icin gerekli olan parametre] "
-        // bizim ornegimiz de " }.[props.product] "
-        // anlami ise " props.product i izle, o DOM a yelestigi zaman artik setProduct islemini bitirebilirsin"  
-    },[props.product]);
-
-    function  handleChange(event){
-        const {name,value} = event.target;
-
-        setProduct(previousProduct => ({
-            ...previousProduct,
-            [name]: name === "categoryId" ? parseInt(value,10) : value
-        }));
+  useEffect(() => {
+    if (categories.length === 0) {
+      getCategories();
     }
+    setProduct({ ...props.product });
+    // useEffect life cycle da ki butun herseyi yonetmeye calisiyor. ve Sonsuz bir donguye giriyor
+    // sonsuz donguden kurtulmak icin " }.[sonsuz donguden kurtarmak icin gerekli olan parametre] "
+    // bizim ornegimiz de " }.[props.product] "
+    // anlami ise " props.product i izle, o DOM a yelestigi zaman artik setProduct islemini bitirebilirsin"
+  }, [props.product]);
 
-    function handleSave(event){
-        event.preventDefault();
-        saveProduct(product).then(()=>{
-            history.push("/")
-        });
-    }
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setProduct((previousProduct) => ({
+      ...previousProduct,
+      [name]: name === "categoryId" ? parseInt(value, 10) : value,
+    }));
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    saveProduct(product).then(() => {
+      history.push("/");
+    });
+  }
+
+  return (
+    // uygulama icerisindeki kullanicagimiz sayfa yazimi
+    <div></div>
+  )
 }
+
+export function getProductById(products, productId) {
+  let product = products.find((product) => product.id === productId) || null;
+  return product;
+}
+
+function mapStateToProps(state, ownProps) {
+  const productId = ownProps.match.pramas.productId;
+  const product =
+    productId && state.productReducer.length > 0
+      ? getProductById(state.productReducer, productId)
+      : {}; // eger bir product var ve product guncelleniyorsa o id ye uygu product ti ver  o yoksa yeni bir product ekliyor  product ti bos bir object ile dondur 
+
+  return {
+    // olusturmak istedigimiz state ler
+    product,
+    products: state.productReducer,
+    categories: state.categoryReducer,
+  };
+}
+
+const mapDispatchToProps = {
+  getCategories,
+  saveProduct,
+  // react hooks tan once bu islem icin bir suru code yaziyorduk. Ama react hook sayesinde yukaridaki gibi sade bir yazim ile islemi hallediyoruz
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddorUpdateProduct);
