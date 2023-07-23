92a9b1dc701c68297174f781a4ec0fca91574d3e
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-07-23 22:07:06 +0300

    daily js

---
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/myMind.js
@@ -0,0 +1,23 @@
+
+// ----------------------------------------------
+
+// const FCC = "freeCodeCamp"; // Change this line
+// let fact = "is cool!"; // Change this line
+// fact = "is awesome!";
+//console.log(FCC, fact); // Change this line
+
+// ----------------------------------------------
+
+// ----------------------------------------------
+
+// let a = 3;
+// let b = 17;
+// let c = 12;
+
+// // Only change code below this line
+// a += 12;
+// b += 9;
+// c += 7;
+// console.log(a,b,c)
+
+// ----------------------------------------------
