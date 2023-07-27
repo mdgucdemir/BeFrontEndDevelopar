8418360060700d58de3796fe8a2ae31c03d6af03
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-07-28 02:38:00 +0300

    daily js

--- UDEMY FRONT END COURSE/Practices/Mixed/my mind/myMind.js
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/myMind.js
@@ -78,3 +78,17 @@
 // console.log(myDog)
 
 // ----------------------------
+
+// const ourDog = {
+//   "name": "Camper",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["everything!"],
+//   "bark": "bow-wow"
+// };
+
+// delete ourDog.bark;
+
+// console.log(ourDog);
+
+//-------------------------------------
