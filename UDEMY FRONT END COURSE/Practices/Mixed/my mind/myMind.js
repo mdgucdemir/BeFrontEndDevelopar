42ae8f922692b1a9d4874ffcfe71dc640f961eb5
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-07-30 22:11:05 +0300

    daily js

--- UDEMY FRONT END COURSE/Practices/Mixed/my mind/myMind.js
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/myMind.js
@@ -92,3 +92,24 @@
 // console.log(ourDog);
 
 //-------------------------------------
+
+// const myArray = [];
+// let i = 0;
+
+// while(i<5){
+//     myArray.push(i);
+//     i++;
+// }
+// console.log(myArray);
+
+// ---------------------------------
+
+const arr = [
+    [1, 2], [3, 4], [5, 6]
+  ];
+  
+  for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+          console.log(arr[i][j]);
+    }
+  }
