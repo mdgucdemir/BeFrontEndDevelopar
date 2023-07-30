1ee496726a7f61b7e91704def256bad8c3d36ebd
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-07-31 00:33:42 +0300

    daily js

--- UDEMY FRONT END COURSE/Practices/Mixed/my mind/myMind.js
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/myMind.js
@@ -104,12 +104,60 @@
 
 // ---------------------------------
 
-const arr = [
-    [1, 2], [3, 4], [5, 6]
-  ];
+// const arr = [
+//     [1, 2], [3, 4], [5, 6]
+//   ];
   
-  for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr[i].length; j++) {
-          console.log(arr[i][j]);
-    }
-  }
+//   for (let i = 0; i < arr.length; i++) {
+//       for (let j = 0; j < arr[i].length; j++) {
+//           console.log(arr[i][j]);
+//     }
+//   }
+
+// -----------------------------
+
+// // Setup
+// const contacts = [
+//     {
+//       firstName: "Akira",
+//       lastName: "Laine",
+//       number: "0543236543",
+//       likes: ["Pizza", "Coding", "Brownie Points"],
+//     },
+//     {
+//       firstName: "Harry",
+//       lastName: "Potter",
+//       number: "0994372684",
+//       likes: ["Hogwarts", "Magic", "Hagrid"],
+//     },
+//     {
+//       firstName: "Sherlock",
+//       lastName: "Holmes",
+//       number: "0487345643",
+//       likes: ["Intriguing Cases", "Violin"],
+//     },
+//     {
+//       firstName: "Kristian",
+//       lastName: "Vos",
+//       number: "unknown",
+//       likes: ["JavaScript", "Gaming", "Foxes"],
+//     },
+//   ];
+  
+//   function lookUpProfile(name, prop) {
+//     // Only change code below this line
+  
+//     for (let i = 0; i < contacts.length; i++) {
+//       if (contacts[i].firstName === name) {
+//         return contacts[i][prop] || "No such contact"
+//       }
+//     }
+  
+//     // Only change code above this line
+//   }
+  
+//   let data = lookUpProfile("Akira", "likes");
+  
+//   console.log(data);
+
+  // ------------------------------------------
