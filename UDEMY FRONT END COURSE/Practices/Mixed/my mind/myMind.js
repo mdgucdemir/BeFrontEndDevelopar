f369fe8c9ba2379c0af09b24b5a0d56e56165cf9
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-07-31 03:07:37 +0300

    dail js

--- UDEMY FRONT END COURSE/Practices/Mixed/my mind/myMind.js
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/myMind.js
@@ -170,6 +170,33 @@
 
 // ------------------------------------
 
-let y = Math.floor(1.6);
-let x  = Math.round(1.6);
-console.log(x,y);
+// let y = Math.floor(-1.3);
+// let x  = Math.round(-1.3);
+// console.log(x,y);
+
+// -----------------------------
+
+// const a = parseInt('11',25);
+// console.log(a);
+
+// ---------------------------
+
+// function countup(n) {
+//     if (n < 1) {
+//       return [];
+//     } else {
+//       const countArray = countup(n - 1);
+//       countArray.push(n);
+//       return countArray;
+//     }
+//   }
+//   console.log(countup(10));
+
+  // ----------------------
+
+  function countdown (n) {
+    return n < 1 ? [] : [n].concat(countdown(n-1));
+  }
+  console.log(countdown(4))
+
+  
