0c8d29525a6b7e8e1765be85fd4385560d6b20a0
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-08-08 02:40:32 +0300

    daily js

--- UDEMY FRONT END COURSE/Practices/Mixed/my mind/myEnjoy.js
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/myEnjoy.js
@@ -1,4 +1,37 @@
-let receivables = 10;
-let payables = 8;
-let netWorkingCapital = receivables - payables;
-console.log(`Net working capital is: ${netWorkingCapital}`);
+// let receivables = 10;
+// let payables = 8;
+// let netWorkingCapital = receivables - payables;
+// console.log(`Net working capital is: ${netWorkingCapital}`);
+
+////////////////////////////////////////////////////
+
+// const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it.';
+// const grouchoContraction = "I've had a perfectly wonderful evening, but this wasn't it.";
+// const quoteInString = "Groucho Marx once said 'Quote me as saying I was mis-quoted.'";
+// //console.log(allSameQuotes);
+// console.log(quoteInString);
+
+////////////////////////////////////////////////////
+
+// function myFunction() {
+//     return "You rock!";
+//   }
+// let varOne = myFunction;
+//  let varTwo = myFunction();
+
+//  //console.log(varOne);
+//  console.log(varTwo);
+
+///////////////////////////////
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+let len = alphabet.length;
+for (let i = 0; i <= len; i++) {
+  console.log(alphabet[i]);
+}
+for (let j = 1; j < len; j++) {
+  console.log(alphabet[j]);
+}
+for (let k = 0; k < len; k++) {
+  console.log(alphabet[k]);
+}
