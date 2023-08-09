fbcf307d5883f8b04898328947fa73da00ac8749
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-08-09 19:28:43 +0300

    daily js

--- UDEMY FRONT END COURSE/Practices/Mixed/my mind/myEnjoy.js
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/myEnjoy.js
@@ -100,3 +100,20 @@
 // console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
 
 //////////////////////////////////
+
+///// Copy an Array with the Spread Operator /////
+
+// function copyMachine(arr, num) {
+//     let newArr = [];
+//     while (num >= 1) {
+//       // Only change code below this line
+//   newArr.push([...arr]);
+//       // Only change code above this line
+//       num--;
+//     }
+//     return newArr;
+//   }
+  
+//   console.log(copyMachine([true, false, true], 2));
+
+  /////////////////////////////////////////////////
