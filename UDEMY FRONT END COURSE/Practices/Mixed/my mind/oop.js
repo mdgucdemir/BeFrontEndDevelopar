055f8465730c8ec4f5897427ba82623d680c4e14
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-08-16 21:56:09 +0300

    daily js

---
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/oop.js
@@ -0,0 +1,30 @@
+
+/////////////
+
+// let duck = {
+//     name: "Aflac",
+//     numLegs: 2,
+//     sayName: function() {return "The name of this duck is " + duck.name + ".";}
+//   };
+//   duck.sayName();
+
+//////////////////////////////
+
+//   let duck = {
+//     name: "Aflac",
+//     numLegs: 2,
+//     sayName: function() {return "The name of this duck is " + this.name + ".";}
+//   };
+
+ /////////////////////////////
+
+////////// Define a Constructor Function ///////////////
+// if you write Capitalize a function name like below example, this is define a Constructor function
+
+// function Bird() {
+//     this.name = "Albert";
+//     this.color = "blue";
+//     this.numLegs = 2;
+//   }
+
+///////////////
