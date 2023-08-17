b16468ca71b73f77a58285c0b562f51efaf72166
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-08-18 00:24:25 +0300

    daily js

--- UDEMY FRONT END COURSE/Practices/Mixed/my mind/oop.js
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/oop.js
@@ -55,3 +55,71 @@
 //   let crow = new Bird("Alexis", "black");
   
 //   console.log(crow instanceof Bird);
+
+////////////////////////////////////
+
+// function Bird(name) {
+//     this.name = name;
+//     this.numLegs = 2;
+//   }
+  
+//   let canary = new Bird("Tweety");
+//   let ownProps = [];
+//   // Only change code below this line
+  
+//   for(let property in canary){
+//     if(canary.hasOwnProperty(property)){
+//       ownProps.push(property)
+//     }
+// }
+  
+// console.log(ownProps);
+
+///////////////////////////////////////
+
+
+
+///////// use prototype /////////////
+
+// function Dog(name) {
+//     this.name = name;
+//   }
+  
+//   Dog.prototype.numLegs = 4;
+  
+//   // Only change code above this line
+//   let beagle = new Dog("Snoopy");
+  
+//   console.log(beagle.numLegs);
+
+/////////////////////////////
+
+
+
+///////////////////////// 
+
+// function Dog(name) {
+//     this.name = name;  // own property
+//   }
+  
+//   Dog.prototype.numLegs = 4;  // prototype property
+  
+//   let beagle = new Dog("Snoopy");
+  
+//   let ownProps = [];
+//   let prototypeProps = [];
+  
+//   // Only change code below this line
+  
+//   for(let property in beagle) {
+//     if(beagle.hasOwnProperty(property)){
+//       ownProps.push(property)
+//     } else {
+//       prototypeProps.push(property)
+//     }
+//   }
+  
+//   console.log(ownProps);
+//   console.log(prototypeProps);
+
+//////////////////////////////////
