2ea3922de8b4e59c25b6df1f1bcbba3f351f5435
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-08-18 18:17:49 +0300

    daily js

--- UDEMY FRONT END COURSE/Practices/Mixed/my mind/oop.js
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/oop.js
@@ -177,3 +177,24 @@
 // beagle.eat()
 
 //////////////////////////////////
+
+
+
+///// Add Methods After Inheritance /////
+
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function () {
+  console.log('Woof!');
+}
+// Only change code above this line
+
+let beagle = new Dog();
+beagle.eat();   // nom nom nom
+beagle.bark();  // Woof!
