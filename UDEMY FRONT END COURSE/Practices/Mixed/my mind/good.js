b3d506a1bf4e6ee8a667f605ef72eb7c533c8283
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-08-19 03:15:02 +0300

    daily.js

---
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/good.js
@@ -0,0 +1,16 @@
+
+/////////////////////////
+
+// function sumAll(arr) {
+//     let sum = 0;
+//     for(let i = Math.min(...arr); i <= Math.max(...arr); i++){
+//       sum += i;
+//     }
+//     return sum;
+//   }
+  
+//   let result = sumAll([1, 4]);
+//   console.log(result);
+
+////////////////////////////
+
