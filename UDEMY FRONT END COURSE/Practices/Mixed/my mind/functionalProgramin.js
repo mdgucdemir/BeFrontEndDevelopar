a0063d6bdffeadcf7fc881374a09c2312a95b37b
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-08-20 19:08:23 +0300

    daily js

--- UDEMY FRONT END COURSE/Practices/Mixed/my mind/functionalProgramin.js
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/functionalProgramin.js
@@ -29,3 +29,26 @@ Functional programming is about:
 // const tea4TeamFCC = getTea(40);
 
 ///////////////////////////////////
+
+// // The global variable
+// const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// // Change code below this line
+// function add(list, bookName) {
+//   return [...list, bookName];  
+//   // Change code above this line
+// }
+
+// // Change code below this line
+// function remove(list, bookName) {  
+//     return list.filter(book => book !== bookName);
+//     // Change code above this line    
+// }
+
+// var newBookList = add(bookList, 'A Brief History of Time');
+// var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+// var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+// console.log(bookList);
+
+//////////////////////////////////
