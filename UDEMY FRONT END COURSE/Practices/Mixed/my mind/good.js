d623bbbd623373c9f1209be169cb4ce2a62de7e0
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-08-21 18:16:35 +0300

    daily js

--- UDEMY FRONT END COURSE/Practices/Mixed/my mind/good.js
+++ UDEMY FRONT END COURSE/Practices/Mixed/my mind/good.js
@@ -14,3 +14,23 @@
 
 ////////////////////////////
 
+// const fruits = ['apple', 'banana', 'orange', 'grape'];
+
+// const hasBanana = fruits.includes('banana');
+// console.log(hasBanana); // true
+
+// const hasPineapple = fruits.includes('pineapple');
+// console.log(hasPineapple); // false
+
+/////////////////////////
+
+// function diffArray(arr1, arr2) {
+//     return arr1
+//     .concat(arr2)
+//     .filter(item => !arr1.includes(item) || !arr2.includes(item));
+// }
+  
+// let result = diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+// console.log(result);
+
+/////////////////////////////
