7b36148a84e88986fc8a3a1a27f6963fba97e9e0
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-10-03 01:03:39 +0300

    learning typescript

--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-4/dist/app.js
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-4/dist/app.js
@@ -1,6 +1,42 @@
 "use strict";
-// // Including & Excluding Files
-const button = document.querySelector("button");
-button.addEventListener('click', () => {
-    console.log('Clicked!');
-});
+// // Code goes here
+// // const - let - any  (differences)
+// // -- function -- 
+// // const add = (a: number, b: number) => {
+// //     return a + b;
+// // }
+// const add = (a: number, b: number) => a + b; // one output
+// // const printOutput = (output: number | string) => {
+// //     console.log(output)
+// // }
+// const printOutput: (a: number | string) => void = output => console.log(output);
+// const button = document.querySelector("button");
+// if(button) {
+//     button.addEventListener('click', event => console.log(event))
+// }
+// // -- Spread Operator
+const hobbies = ['sport', 'cooking'];
+const activeHobbies = ['hiking'];
+activeHobbies.push(...hobbies); // spread operator for array
+const person = {
+    firstName: 'M.Deniz',
+    age: 33
+};
+const copiedPerson = Object.assign({}, person); // spread operator for object
+// // -- Rest Parameters
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    });
+};
+const addedNumbers = add(5, 2, 10, 3.7);
+console.log(addedNumbers);
+// // --- Array & Object Destructuring ---
+// // Destructuring doesn't change original Array and Object.
+// // - Array Destructuring -
+const [hobby1, hobby2, ...remainingHobbies] = hobbies; // this is destructuring  // (hobbies[0] = hobby1) and (hobbies[1] = hobby2) and (hobbies[remaining] = remainingHobbies)
+console.log(hobbies, hobby1, hobby2);
+// // - Object Destructuring -
+const { firstName: username, age } = person;
+console.log(username, age);
+console.log(username, age, person);
