e25bb5159c69f0b801989068b318c6f4f1582dcb
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-10-06 20:52:21 +0300

    daily ts

--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-6/dist/app.js
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-6/dist/app.js
@@ -1,8 +1,64 @@
 "use strict";
-// // --- Intersection Types ---
 const employee1 = {
-    name: 'Mehmet deniz',
-    privileges: ['admin', 'to many'],
-    date: new Date()
+    name: "Mehmet deniz",
+    privileges: ["admin", "to many"],
+    startDate: new Date(),
 };
 console.log(employee1);
+// // More on Type Guards
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("Start Date: " + emp.startDate);
+    }
+}
+printEmployeeInformation(employee1);
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving a truck...");
+    }
+    loadCargo(amount) {
+        console.log("Loading Cargo ... " + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    // if('loadCargo' in vehicle) {
+    //     vehicle.loadCargo(1000);
+    // }
+    // // other way
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+    }
+    console.log('Moving speed is: ' + speed);
+}
+moveAnimal({ type: 'bird', flyingSpeed: 10 });
+// // --- Type Casting ---
--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-6/src/app.ts
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-6/src/app.ts
@@ -1,23 +1,96 @@
-
-
 // // --- Intersection Types ---
-
 type Admin = {
-    name: string;
-    privileges: string[];
-}
-
+  name: string;
+  privileges: string[];
+};
 type Employee = {
-    name: string;
-    date: Date;
-}
-
-type ElevatedEmployee = Admin & Employee;
-
+  name: string;
+  startDate: Date;
+};
+type ElevatedEmployee = Admin & Employee; // this is intersection type
 const employee1: ElevatedEmployee = {
-    name: 'Mehmet deniz',
-    privileges: ['admin','to many'],
-    date: new Date()
-}
-
+  name: "Mehmet deniz",
+  privileges: ["admin", "to many"],
+  startDate: new Date(),
+};
 console.log(employee1);
+// // and also we can...
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
+
+// // More on Type Guards
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+type UnknownEmployee = Employee | Admin;
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+printEmployeeInformation(employee1);
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading Cargo ... " + amount);
+  }
+}
+type Vehicle = Car | Truck;
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // if('loadCargo' in vehicle) {
+  //     vehicle.loadCargo(1000);
+  // }
+  // // other way
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+useVehicle(v1);
+useVehicle(v2);
+
+// // --- Discriminated Unions ---
+
+interface Bird {
+    type: 'bird';
+    flyingSpeed: number;
+}
+interface Horse {
+    type: 'horse';
+    runningSpeed: number;
+}
+type Animal = Bird | Horse;
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch(animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+    }
+    console.log('Moving speed is: ' + speed);
+}
+moveAnimal({type:'bird', flyingSpeed:10});
+
+
+// // --- Type Casting ---
