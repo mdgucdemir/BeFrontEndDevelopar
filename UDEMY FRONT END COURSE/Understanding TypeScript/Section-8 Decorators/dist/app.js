eddbc2dcea20d5ab18b21dedf29366338b2e39c5
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-10-08 21:56:06 +0300

    learning typescript

--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/dist/app.js
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/dist/app.js
@@ -1,2 +1,73 @@
 "use strict";
 // // --- Decorators --- \\ \\
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// // --- First class decorator ---
+// function Logger(constructor: Function) {
+//     console.log('Logging...');
+//     console.log(constructor);
+// }
+// // --- Working with Decorator Factories ---
+function Logger(logString) {
+    console.log('This is `Logger Console`.');
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
+}
+// // --- Building Most Useful Decorators ---
+function WithTemplate(tamplate, hookId) {
+    console.log('This is `With Template Console`.');
+    // return function(_:Function) {
+    return function (constructor) {
+        console.log('Faso Fosik');
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = tamplate;
+            hookEl.querySelector('h1').textContent = p.name;
+        }
+    };
+}
+let Person = class Person {
+    constructor() {
+        this.name = "Deniz";
+        console.log('Creating Person Object...');
+    }
+};
+Person = __decorate([
+    Logger('LOGGING-PERSON') // this is decorator. If you want make decorator you must use this symbol ' @ '.
+    ,
+    WithTemplate('<h1>My Person Object</h1>', 'app')
+], Person);
+const pers = new Person();
+console.log(pers);
+// // --- Diving into Property Decorators ---
+function Log(target, propertyName) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+class Product {
+    set price(val) {
+        if (val) {
+            this._price = val;
+        }
+        else {
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/src/app.ts
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/src/app.ts
@@ -1,1 +1,75 @@
 // // --- Decorators --- \\ \\
+
+// // --- First class decorator ---
+// function Logger(constructor: Function) {
+//     console.log('Logging...');
+//     console.log(constructor);
+// }
+
+// // --- Working with Decorator Factories ---
+function Logger(logString: string) {
+    console.log('This is `Logger Console`.');
+
+    return function(constructor: Function) {
+       console.log(logString);
+       console.log(constructor); 
+    }    
+}
+
+// // --- Building Most Useful Decorators ---
+function WithTemplate(tamplate: string, hookId: string) {
+    console.log('This is `With Template Console`.');
+    // return function(_:Function) {
+        return function(constructor: any) {
+        console.log('Faso Fosik');
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = tamplate;
+            hookEl.querySelector('h1')!.textContent = p.name;
+        }
+    }
+}
+@Logger('LOGGING-PERSON') // this is decorator. If you want make decorator you must use this symbol ' @ '.
+@WithTemplate('<h1>My Person Object</h1>','app')
+class Person {
+    name = "Deniz";
+
+    constructor() {
+        console.log('Creating Person Object...');
+    }
+}
+const pers = new Person();
+console.log(pers)
+
+
+// // --- Diving into Property Decorators ---
+
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    set price(val: number) {
+        if(val) {
+            this._price = val;
+        }
+        else {
+            throw new Error ('Invalid price - should be positive!');
+        }
+        
+    }   
+
+    constructor(t: string , p: number) {
+        this.title = t;
+        this._price = p;
+    }
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+    }
+}
