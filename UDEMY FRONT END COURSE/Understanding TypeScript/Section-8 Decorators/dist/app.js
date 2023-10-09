4edcd55462067635c43c9e7d07fa9cf4ff978b0e
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-10-10 01:27:40 +0300

    learning typescript

--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/dist/app.js
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/dist/app.js
@@ -105,3 +105,82 @@ __decorate([
     Log3,
     __param(0, Log4)
 ], Product.prototype, "getPriceWithTax", null);
+function Autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundfn = originalMethod.bind(this);
+            return boundfn;
+        },
+    };
+    return adjDescriptor;
+}
+class Printer {
+    constructor() {
+        this.message = 'This works!';
+    }
+    showMessage() {
+        console.log(this.message);
+    }
+}
+__decorate([
+    Autobind
+], Printer.prototype, "showMessage", null);
+const p = new Printer();
+const button = document.querySelector('button');
+// button.addEventListener('click', p.showMessage.bind(p));
+button.addEventListener('click', p.showMessage);
+const registeredValidators = {};
+function Required(target, propName) {
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: ['required'] });
+}
+;
+function PositiveNumber(target, propName) {
+    registeredValidators[target.constructor.name] = Object.assign(Object.assign({}, registeredValidators[target.constructor.name]), { [propName]: ['positive'] });
+}
+function validate(obj) {
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    if (!objValidatorConfig) {
+        return true;
+    }
+    let isValid = true;
+    for (const prop in objValidatorConfig) {
+        for (const validator of objValidatorConfig[prop]) {
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop]; // !! isereti true-false degerine dondurmek icin
+                case 'positive':
+                    isValid = isValid && obj[prop] > 0;
+            }
+        }
+    }
+    return isValid;
+}
+class Course {
+    constructor(t, p) {
+        this.title = t;
+        this.price = p;
+    }
+}
+__decorate([
+    Required
+], Course.prototype, "title", void 0);
+__decorate([
+    PositiveNumber
+], Course.prototype, "price", void 0);
+const courseForm = document.querySelector('form');
+courseForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const titleEl = document.getElementById('title');
+    const priceEl = document.getElementById('price');
+    const title = titleEl.value;
+    const price = +priceEl.value;
+    const createCourse = new Course(title, price);
+    if (!validate(createCourse)) {
+        alert('Invalid input, please try again');
+        return;
+    }
+    console.log(createCourse);
+});
--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/src/app.ts
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/src/app.ts
@@ -98,3 +98,97 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundfn = originalMethod.bind(this);
+            return boundfn;
+        },
+    };
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'This works!';
+
+    @Autobind
+    showMessage() {
+        console.log(this.message);
+    }    
+}
+const p = new Printer()
+const button = document.querySelector('button')!;
+// button.addEventListener('click', p.showMessage.bind(p));
+button.addEventListener('click', p.showMessage);
+
+
+// // --- Validation with Decorators ---
+interface ValidatorConfig {
+    [property: string]: {
+        [validatableProp: string]: string[] // ["required", "positive"]
+    }
+}
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+        ...registeredValidators[target.constructor.name],
+        [propName]: ['required']
+    };
+};
+
+function PositiveNumber(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+        ...registeredValidators[target.constructor.name],
+        [propName]: ['positive']
+    };
+}
+function validate(obj: any) {
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    if(!objValidatorConfig) {
+        return true;
+    }
+    let isValid = true;
+    for (const prop in objValidatorConfig) {        
+        for(const validator of objValidatorConfig[prop]) {
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop]; // !! isereti true-false degerine dondurmek icin
+                case 'positive':
+                    isValid = isValid && obj[prop] > 0;                
+            }
+        }
+    }
+    return isValid;
+}
+
+class Course {
+    @Required
+    title: string;
+    @PositiveNumber
+    price: number;
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this.price = p;
+    }
+}
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const titleEl = document.getElementById('title') as HTMLInputElement;
+    const priceEl = document.getElementById('price') as HTMLInputElement;
+    const title = titleEl.value;
+    const price = +priceEl.value;
+
+    const createCourse = new Course(title, price);
+    if (!validate(createCourse)) {
+        alert('Invalid input, please try again');
+        return;
+    }
+    console.log(createCourse);
+});
