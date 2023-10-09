376c815a0cdb51dcbf727875bf5eacd2d25d0676
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-10-09 19:29:26 +0300

    daily ts

--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/dist/app.js
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/dist/app.js
@@ -6,6 +6,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 // // --- First class decorator ---
 // function Logger(constructor: Function) {
 //     console.log('Logging...');
@@ -51,6 +54,25 @@ function Log(target, propertyName) {
     console.log('Property decorator!');
     console.log(target, propertyName);
 }
+// // --- Accesor & Parameter Decorators ---
+function Log2(target, name, descriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target, name, position) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
 class Product {
     set price(val) {
         if (val) {
@@ -71,3 +93,10 @@ class Product {
 __decorate([
     Log
 ], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/src/app.ts
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-8 Decorators/src/app.ts
@@ -44,32 +44,50 @@ console.log(pers)
 
 
 // // --- Diving into Property Decorators ---
-
 function Log(target: any, propertyName: string | Symbol) {
     console.log('Property decorator!');
     console.log(target, propertyName);
 }
 
+// // --- Accesor & Parameter Decorators ---
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);    
+}
+function Log3(target: any, name:string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor); 
+}
+function Log4 (target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position); 
+}
 class Product {
     @Log
     title: string;
     private _price: number;
 
+    @Log2
     set price(val: number) {
         if(val) {
             this._price = val;
-        }
-        else {
+        } else {
             throw new Error ('Invalid price - should be positive!');
         }
         
     }   
-
     constructor(t: string , p: number) {
         this.title = t;
         this._price = p;
     }
-    getPriceWithTax(tax: number) {
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
         return this._price * (1 + tax);
     }
 }
