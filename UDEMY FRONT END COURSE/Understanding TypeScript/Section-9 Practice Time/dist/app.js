d8a329f233f09d3c4ecbf64df281c1b823c861cf
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-10-14 00:03:51 +0300

    daily ts

--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-9 Practice Time/dist/app.js
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-9 Practice Time/dist/app.js
@@ -99,20 +99,39 @@ class Component {
 }
 // ProjectItem Class
 class ProjectItem extends Component {
+    get persons() {
+        if (this.project.people === 1) {
+            return '1 person';
+        }
+        else {
+            return `${this.project.people} persons`;
+        }
+    }
     constructor(hostId, project) {
         super('single-project', hostId, false, project.id);
         this.project = project;
         this.configure();
         this.renderContent();
     }
+    dragStartHandler(event) {
+        console.log(event);
+    }
+    dragEndHandler(_) {
+        console.log('dragEnd');
+    }
     configure() {
+        this.element.addEventListener('dragstart', this.dragStartHandler);
+        this.element.addEventListener('dragend', this.dragEndHandler);
     }
     renderContent() {
         this.element.querySelector('h2').textContent = this.project.title;
-        this.element.querySelector('h3').textContent = this.project.people.toString();
+        this.element.querySelector('h3').textContent = this.persons + ' assigned';
         this.element.querySelector('p').textContent = this.project.description;
     }
 }
+__decorate([
+    autobind
+], ProjectItem.prototype, "dragStartHandler", null);
 // ProjectList Class
 class ProjectList extends Component {
     constructor(type) {
--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-9 Practice Time/src/app.ts
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-9 Practice Time/src/app.ts
@@ -1,5 +1,17 @@
 // // --- Prictice Time --- \\ \\
 
+// Drag & Drop Interfaces
+interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent) : void;
+}
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+}
+
 // Project Type
 enum ProjectStatus {Active, Finished}
 class Project {
@@ -129,8 +141,16 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectItem Class
-class ProjectItem extends Component<HTMLUListElement,HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement,HTMLLIElement> implements Draggable {
     private project: Project;
+
+    get persons() {
+        if(this.project.people === 1) {
+            return '1 person';
+        } else {
+            return `${this.project.people} persons`;
+        }
+    }
 
     constructor(hostId: string, project: Project) {
         super('single-project', hostId, false, project.id);
@@ -140,13 +160,23 @@ class ProjectItem extends Component<HTMLUListElement,HTMLLIElement> {
         this.renderContent();
     }
 
+    @autobind
+    dragStartHandler(event: DragEvent): void {
+        console.log(event);
+    }
+
+    dragEndHandler(_: DragEvent): void {
+        console.log('dragEnd');
+    }
+
     configure(): void {
-        
+        this.element.addEventListener('dragstart',this.dragStartHandler);
+        this.element.addEventListener('dragend',this.dragEndHandler);
     }
 
     renderContent(): void {
         this.element.querySelector('h2')!.textContent = this.project.title;
-        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
         this.element.querySelector('p')!.textContent = this.project.description;
     }
 }
