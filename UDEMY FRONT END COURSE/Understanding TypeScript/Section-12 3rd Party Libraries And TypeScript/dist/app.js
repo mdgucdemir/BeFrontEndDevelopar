af4ad94a2401336ffcce5e97328360ef88de1af9
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-10-16 22:57:45 +0300

    daily ts

--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-12 3rd Party Libraries And TypeScript/dist/app.js
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-12 3rd Party Libraries And TypeScript/dist/app.js
@@ -1,9 +1,20 @@
-import { Product } from "./product.model";
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { Product } from './product.model';
 const products = [{ title: 'adkf', price: 23 }, { title: 'sdadf', price: 23 }];
-const fosikProduct = products.map(prod => {
-    return new Product(prod.title, prod.price);
-});
+const fosikProduct = plainToClass(Product, products);
 for (const prod of fosikProduct) {
     console.log(prod.getInformation());
 }
+const newProd = new Product('', -2);
+validate(newProd).then(errors => {
+    if (errors.length > 0) {
+        console.log("VALIDATION ERRORS!");
+        console.log(errors);
+    }
+    else {
+        console.log(newProd.getInformation());
+    }
+});
 //# sourceMappingURL=app.js.map
--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-12 3rd Party Libraries And TypeScript/src/app.ts
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-12 3rd Party Libraries And TypeScript/src/app.ts
@@ -1,11 +1,29 @@
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator'
 
-import { Product } from "./product.model";
+import { Product } from './product.model';
 
 const products = [{title: 'adkf',price: 23},{title: 'sdadf', price: 23}];
+
 // const p1 = new Product('A book' , 21);
-const fosikProduct = products.map(prod => {
-    return new Product(prod.title,prod.price);
-});
+
+// const fosikProduct = products.map(prod => {
+//     return new Product(prod.title,prod.price);
+// });
+
+const fosikProduct = plainToClass(Product, products);
+
 for (const prod of fosikProduct) {
     console.log(prod.getInformation());
 }
+
+const newProd = new Product('',-2);
+validate(newProd).then(errors => {
+    if (errors.length > 0) {
+        console.log("VALIDATION ERRORS!");
+        console.log(errors);
+    } else {
+        console.log(newProd.getInformation());
+    }
+});
