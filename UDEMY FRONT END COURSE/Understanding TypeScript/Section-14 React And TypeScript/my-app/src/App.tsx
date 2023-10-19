b66754c097f69087dd8acaf25248b15b20bf03de
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2023-10-20 00:03:12 +0300

    daily ts

--- UDEMY FRONT END COURSE/Understanding TypeScript/Section-14 React And TypeScript/my-app/src/App.tsx
+++ UDEMY FRONT END COURSE/Understanding TypeScript/Section-14 React And TypeScript/my-app/src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
 
-import './App.css';
+
 
 function App() {
+  
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [...prevTodos, {id: Math.random().toString(), text: text}]);
+  }
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos( prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
+  };
+
+
   return (
     <div className="App">
-      
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items = {todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 }
