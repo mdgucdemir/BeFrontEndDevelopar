2d0e57fb846eed87f743d7930a52071b40d9bf0c
Mehmet Deniz Gucdemir <md.gucdemir@gmail.com>
2023-10-27 02:13:16 +0300

    daily working

---
+++ My Projects/metflix/src/app.js
@@ -0,0 +1,26 @@
+const rightArrows = document.querySelectorAll(".bi-chevron-right");
+const leftArrows = document.querySelectorAll(".bi-chevron-left");
+const movieLists = document.querySelectorAll(".movie-list");
+
+// console.log(movieLists)
+
+// right arrows
+rightArrows.forEach((arrow, i) => {
+  let clickCounter = 0;
+  const imageItem = movieLists[i].querySelectorAll("img").length;
+  arrow.addEventListener("click", function () {
+    clickCounter++;
+    if (imageItem - (4 + clickCounter) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+    }
+  });
+});
+
+// left arrows
+leftArrows.forEach((arrow, i) => {   
+    
+  });
