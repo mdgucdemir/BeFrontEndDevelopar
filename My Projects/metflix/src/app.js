44f10355b9bcd704740b5de56cfe4725273012af
Mehmet Deniz Gucdemir <md.gucdemir@gmail.com>
2023-10-27 19:48:14 +0300

    daily working

--- My Projects/metflix/src/app.js
+++ My Projects/metflix/src/app.js
@@ -4,23 +4,38 @@ const movieLists = document.querySelectorAll(".movie-list");
 
 // console.log(movieLists)
 
+
 // right arrows
 rightArrows.forEach((arrow, i) => {
   let clickCounter = 0;
   const imageItem = movieLists[i].querySelectorAll("img").length;
   arrow.addEventListener("click", function () {
     clickCounter++;
-    if (imageItem - (4 + clickCounter) >= 0) {
+    if (imageItem - (2 + clickCounter) >= 0) {
       movieLists[i].style.transform = `translateX(${
         movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
       }px)`;
     } else {
       movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
     }
   });
 });
 
 // left arrows
-leftArrows.forEach((arrow, i) => {   
-    
+leftArrows.forEach((arrow, i) => {
+  let clickCounter = 0;
+  const imageItem = movieLists[i].querySelectorAll("img").length;
+  arrow.addEventListener("click", function () {
+    clickCounter++;
+    if (imageItem - (2 + clickCounter) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value + 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
   });
+
+});
