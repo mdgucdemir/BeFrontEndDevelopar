9319bd7a39d2bd0b91d09056ce1b14c328cf23ff
Mehmet Deniz Gucdemir <md.gucdemir@gmail.com>
2023-11-04 19:26:55 +0300

    daily working

--- My Projects/movie-website-project/src/pages/home/Home.jsx
+++ My Projects/movie-website-project/src/pages/home/Home.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './style.scss'
-import HeroBanner from './heroBanner/HeroBanner';
-
+import React from "react";
+import "./style.scss";
+import HeroBanner from "./heroBanner/HeroBanner";
 
 const Home = () => {
   return (
-    <div className='homePage'>
+    <div className="homePage">
       <HeroBanner />
+      <div style={{ height: 1000 }}></div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
