f50cccd66323c41ec7d2b7735b8a0f3efadfb625
Mehmet Deniz Gucdemir <md.gucdemir@gmail.com>
2024-01-08 00:28:57 +0300

    daily angular

--- UDEMY FRONT END COURSE/Modern web gelistirme kursu/39-) Angular/my-first-angular-project/src/app/app.module.ts
+++ UDEMY FRONT END COURSE/Modern web gelistirme kursu/39-) Angular/my-first-angular-project/src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductComponent } from './product/product.component';
 import { CategoryComponent } from './category/category.component';
+import { ProductFilterPipe } from './product/product-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductComponent,
-    CategoryComponent
+    CategoryComponent,
+    ProductFilterPipe,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
