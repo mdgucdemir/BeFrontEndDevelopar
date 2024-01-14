dd47c2bec9f1a14953c76a47bce70070f1914341
Mehmet Deniz Gucdemir <md.gucdemir@gmail.com>
2024-01-14 20:40:54 +0300

    daily angular

--- UDEMY FRONT END COURSE/Angular Kursu/section-3/src/app/app.component.ts
+++ UDEMY FRONT END COURSE/Angular Kursu/section-3/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'section-3';
+  randomText = faker.lorem.sentence();
+
+  enteredText = '';
+
+  // -- event.target.value -- \\
+  eventTargetValue(value: string) {
+    this.enteredText = value;
+  }
+  getInputValue(event: Event) {
+    const inputValue = (event.target as HTMLInputElement)?.value;
+    this.eventTargetValue(inputValue);
+  }
+  // -- event.target.value END -- \\
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'currect' : 'incorrect';
+  }
 }
