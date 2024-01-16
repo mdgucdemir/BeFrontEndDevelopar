e88ffbb4d9fb9666d856ce31ce24db5a57236423
Mehmet Deniz Gucdemir <md.gucdemir@gmail.com>
2024-01-16 04:40:51 +0300

    daily angular

--- UDEMY FRONT END COURSE/Angular Kursu/section-6/src/app/app.module.ts
+++ UDEMY FRONT END COURSE/Angular Kursu/section-6/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstDirective } from './first.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FirstDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
