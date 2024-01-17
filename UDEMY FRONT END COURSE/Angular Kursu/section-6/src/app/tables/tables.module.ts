af5fb6e7fdfe8cfbadb70ede703e469e3ef966e1
Mehmet Deniz Gucdemir <md.gucdemir@gmail.com>
2024-01-18 02:29:47 +0300

    daily angular

--- UDEMY FRONT END COURSE/Angular Kursu/section-6/src/app/tables/tables.module.ts
+++ UDEMY FRONT END COURSE/Angular Kursu/section-6/src/app/tables/tables.module.ts
@@ -8,9 +8,10 @@ import { SharedModule } from '../shared/shared.module';
 import { BiographyComponent } from './biography/biography.component';
 import { CompaniesComponent } from './companies/companies.component';
 import { PartnersComponent } from './partners/partners.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
-  declarations: [TablesHomeComponent, TableComponent, BiographyComponent, CompaniesComponent, PartnersComponent],
+  declarations: [TablesHomeComponent, TableComponent, BiographyComponent, CompaniesComponent, PartnersComponent, TabsComponent],
   imports: [CommonModule, TablesRoutingModule, SharedModule],
 })
 export class TablesModule {}
