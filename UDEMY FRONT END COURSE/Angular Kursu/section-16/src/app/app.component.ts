18231696f40bf1aabb0e7d8258613eba5e657599
Mehmet Deniz Gucdemir <md.gucdemir@gmail.com>
2024-02-03 02:14:58 +0300

    daily angular

--- UDEMY FRONT END COURSE/Angular Kursu/section-16/src/app/app.component.ts
+++ UDEMY FRONT END COURSE/Angular Kursu/section-16/src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
+import { map, startWith } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
 
 export interface Tile {
   color: string;
   cols: number;
   rows: number;
   text: string;
+}
+export interface Backend {
+  name: string;
+}
+
+export interface State {
+  flag: string;
+  name: string;
+  population: string;
 }
 
 @Component({
@@ -14,8 +27,12 @@ export interface Tile {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private cdref: ChangeDetectorRef) {
-    // NG0100: ExpressionChangedAfterItHasBeenCheckedError hatasini onlemek icin
+  constructor() {
+    // NG0100: ExpressionChangedAfterItHasBeenCheckedError hatasini onlemek icin constructor(private cdref: ChangeDetectorRef) {}
+    this.filteredStates = this.stateCtrl.valueChanges.pipe(
+      startWith(''),
+      map((state) => (state ? this._filterStates(state) : this.states.slice()))
+    );
   }
   title = 'section-16';
   news = 10;
@@ -79,10 +96,63 @@ export class AppComponent {
   // Tab material selectedTabChange icin start
   ngAfterContentChecked() {
     // NG0100: ExpressionChangedAfterItHasBeenCheckedError hatasini onlemek icin
-    this.cdref.detectChanges();
+    // this.cdref.detectChanges();
   }
   tabChange(index: number | null) {
     console.log(index);
   }
   // Tab material selectedTabChange icin end
+
+  selectedValue: string = '';
+
+  options: string[] = ['Angular', 'React', 'Vue'];
+  objectOptions = [{ name: 'C#' }, { name: 'Node Js' }, { name: 'Python' }];
+
+  showIt(subject: Backend): string {
+    return subject ? subject.name : '';
+  }
+
+  stateCtrl = new FormControl('');
+  filteredStates: Observable<State[]>;
+  states: State[] = [
+    {
+      name: 'Arkansas',
+      population: '2.978M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg',
+    },
+    {
+      name: 'California',
+      population: '39.14M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg',
+    },
+    {
+      name: 'Florida',
+      population: '20.27M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg',
+    },
+    {
+      name: 'Texas',
+      population: '27.47M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg',
+    },
+  ];
+  private _filterStates(value: string): State[] {
+    const filterValue = value.toLowerCase();
+
+    return this.states.filter((state) =>
+      state.name.toLowerCase().includes(filterValue)
+    );
+  }
+
+  minDate = new Date();
+  maxDate = new Date('feb 20, 2024');
+
+  dateFilter = (date: any) => {
+    const day = date.getDay();
+    return day != 0;
+  };
 }
--- UDEMY FRONT END COURSE/Angular Kursu/section-16/src/app/app.module.ts
+++ UDEMY FRONT END COURSE/Angular Kursu/section-16/src/app/app.module.ts
@@ -19,6 +19,16 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [AppComponent],
@@ -40,6 +50,17 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatExpansionModule,
     MatCardModule,
     MatTabsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
