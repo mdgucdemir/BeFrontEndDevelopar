47713466ac467c55c7e7b374d376f259a4ad24d8
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-02-14 22:09:50 +0300

    daily angular

---
+++ UDEMY FRONT END COURSE/Angular Kursu/section-23/StudentManagement/StudentManagement/Controllers/StudentsController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagement.DomainModels;
+using StudentManagement.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Controllers
+{
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly IMapper mapper;
+        public StudentsController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            this.studentRepository = studentRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public async Task<IActionResult> GetAllStudentsAsync()
+        {
+            var students= await studentRepository.GetStudentsAsync();
+
+            return Ok(mapper.Map<List<Student>>(students));
+        }
+    }
+}
---
+++ UDEMY FRONT END COURSE/Angular Kursu/section-23/StudentManagement/StudentManagement/Repositories/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using StudentManagement.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Repositories
+{
+    public interface IStudentRepository
+    {
+        Task<List<Student>> GetStudentsAsync();
+    }
+}
