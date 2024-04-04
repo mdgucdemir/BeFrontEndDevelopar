6b01cfaa9307e0a4e158030d8850f78ad1bcf1f3
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-04-05 02:23:28 +0300

    daily react

--- UDEMY FRONT END COURSE/React/19- React Material UI/materialui/src/App.tsx
+++ UDEMY FRONT END COURSE/React/19- React Material UI/materialui/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import LessonButton from "./components/LessonButton";
+import LessonButtonGroup from "./components/LessonButtonGroup";
 import Typography from "./components/Typography";
 
 function App() {
   return (
     <div className="App">
       {/* <Typography /> */}
-      <LessonButton />
+      {/* <LessonButton /> */}
+      <LessonButtonGroup />
     </div>
   );
 }
---
+++ UDEMY FRONT END COURSE/React/19- React Material UI/materialui/src/components/LessonButtonGroup.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LessonButtonGroup = () => {
+  return <div>LessonButtonGroup</div>;
+};
+
+export default LessonButtonGroup;
