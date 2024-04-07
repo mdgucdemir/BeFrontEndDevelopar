574d141d1ec7c36e10aee5a089bc543100af61c5
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-04-07 03:50:41 +0300

    daily react

--- UDEMY FRONT END COURSE/React/19- React Material UI/materialui/src/App.tsx
+++ UDEMY FRONT END COURSE/React/19- React Material UI/materialui/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import LessonButton from "./components/LessonButton";
 import LessonButtonGroup from "./components/LessonButtonGroup";
 import LessonRadioGroup from "./components/LessonRadioGroup";
+import LessonSelect from "./components/LessonSelect";
 import LessonTextField from "./components/LessonTextField";
 import RadioExample from "./components/RadioExample";
 import Typography from "./components/Typography";
@@ -14,7 +15,8 @@ function App() {
       {/* <LessonButtonGroup /> */}
       {/* <LessonTextField /> */}
       {/* <LessonRadioGroup /> */}
-      <RadioExample />
+      {/* <RadioExample /> */}
+      <LessonSelect />
     </div>
   );
 }
---
+++ UDEMY FRONT END COURSE/React/19- React Material UI/materialui/src/components/LessonSelect.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LessonSelect = () => {
+  return <div>LessonSelect</div>;
+};
+
+export default LessonSelect;
