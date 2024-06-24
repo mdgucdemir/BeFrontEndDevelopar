df4678e6c939bdc7f1206ea3a0a5ce0c22ed2a90
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-06-25 02:34:13 +0300

    daily next js

---
+++ UDEMY FRONT END COURSE/Next Js/section3/summary-basic-app/app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function MealDetailsPage({ params }) {
+  return (
+    <div>
+      <h1>Meal Detail Page</h1>
+    </div>
+  );
+}
---
+++ UDEMY FRONT END COURSE/Next Js/section3/summary-basic-app/app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function ShareMealPage() {
+  return (
+    <div>
+      <h1>Meals Share Page</h1>
+    </div>
+  );
+}
