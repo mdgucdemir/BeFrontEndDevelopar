e5e7b360faf67344911ab4806fef987e7b9097de
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-07-03 22:26:32 +0300

    daily next js

---
+++ UDEMY FRONT END COURSE/Next Js/section-6-mutating-data/actions/posts.js
@@ -0,0 +1,46 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
+import { revalidatePath } from "next/cache";
+
+export async function createPost(prevState, formData) {
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
+
+  // console.log(title, image, content);
+
+  let errors = [];
+
+  if (!title || title.trim().length === 0) {
+    errors.push("Title is required.");
+  }
+
+  if (!content || content.trim().length === 0) {
+    errors.push("Content is required.");
+  }
+
+  if (!image || image.size === 0) {
+    errors.push("Image is required.");
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
+
+  await storePost({
+    imageUrl: "",
+    title,
+    content,
+    userId: 1,
+  });
+
+  revalidatePath("/", "layout");
+  redirect("/feed");
+}
+
+export async function togglePostLikeStatus(postId) {
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath("/", "layout");
+}
--- UDEMY FRONT END COURSE/Next Js/section-6-mutating-data/components/posts.js
+++ UDEMY FRONT END COURSE/Next Js/section-6-mutating-data/components/posts.js
@@ -1,7 +1,11 @@
+"use client";
+
 import { formatDate } from "@/lib/format";
 import LikeButton from "./like-icon";
+import { togglePostLikeStatus } from "@/actions/posts";
+import { useOptimistic } from "react";
 
-function Post({ post }) {
+function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
@@ -19,7 +23,12 @@ function Post({ post }) {
             </p>
           </div>
           <div>
-            <LikeButton />
+            <form
+              action={action.bind(null, post.id)}
+              className={post.isLiked ? "liked" : ""}
+            >
+              <LikeButton />
+            </form>
           </div>
         </header>
         <p>{post.content}</p>
@@ -29,15 +38,41 @@ function Post({ post }) {
 }
 
 export default function Posts({ posts }) {
-  if (!posts || posts.length === 0) {
+  // like isleminin serverden cevap gelmesini beklemek yerine useOptimistic kullanarak daha hizli ve daha iyi UX yapmis oluruz
+  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(
+    posts,
+    (prevPosts, updatedPostId) => {
+      const updatedPostIndex = prevPosts.findIndex(
+        (post) => post.id === updatedPostId
+      );
+
+      if (updatedPostIndex === -1) {
+        return prevPosts;
+      }
+
+      const updatedPost = { ...prevPosts[updatedPostIndex] };
+      updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+      updatedPost.isLiked = !updatedPost.isLiked;
+      const newPosts = [...prevPosts];
+      newPosts[updatedPostIndex] = updatedPost;
+      return newPosts;
+    }
+  );
+
+  if (!optimisticPosts || optimisticPosts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
+  }
+
+  async function updatedPost(postId) {
+    updateOptimisticPosts(postId);
+    await togglePostLikeStatus(postId);
   }
 
   return (
     <ul className="posts">
-      {posts.map((post) => (
+      {optimisticPosts.map((post) => (
         <li key={post.id}>
-          <Post post={post} />
+          <Post post={post} action={updatedPost} />
         </li>
       ))}
     </ul>
