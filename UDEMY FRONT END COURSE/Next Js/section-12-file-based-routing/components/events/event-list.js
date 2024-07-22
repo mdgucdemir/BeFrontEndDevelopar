e43fe2187b12b5ae72c48d86fd62ce09b0cb39a0
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-07-23 02:15:00 +0300

    daily next js

---
+++ UDEMY FRONT END COURSE/Next Js/section-12-file-based-routing/components/events/event-list.js
@@ -0,0 +1,22 @@
+import EventItem from "./event-item";
+import classes from "./event-list.module.css";
+
+function EventList(props) {
+  const { items } = props;
+  return (
+    <ul className={classes.list}>
+      {items.map((event) => (
+        <EventItem
+          key={event.id}
+          title={event.title}
+          image={event.image}
+          date={event.date}
+          location={event.location}
+          id={event.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default EventList;
---
+++ UDEMY FRONT END COURSE/Next Js/section-12-file-based-routing/components/ui/button.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import classes from "./button.module.css";
+
+export default function Button(props) {
+  if (props.link) {
+    return (
+      <Link href={props.link} className={classes.btn}>
+        {props.children}
+      </Link>
+    );
+  }
+
+  return (
+    <button onClick={props.onClick} className={classes.btn}>
+      {props.children}
+    </button>
+  );
+}
