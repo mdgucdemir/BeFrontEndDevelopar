0b13a615dcca3802e50702a83fb3a17e34741eda
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-08-11 02:00:12 +0300

    daily next js

--- UDEMY FRONT END COURSE/Next Js/section-19-full-blog/components/posts/post-detail/post-content.js
+++ UDEMY FRONT END COURSE/Next Js/section-19-full-blog/components/posts/post-detail/post-content.js
@@ -2,38 +2,64 @@ import Image from "next/image";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
+// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 function PostContent(props) {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
-  // const customRenderers = {
-  //   paragraph(paragraph) {
-  //     const { node } = paragraph;
+  const customComponents = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/image/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
 
-  //     if (node.children[0].type === "image") {
-  //       const image = node.children[0];
+    paragraph(paragraph) {
+      const { node } = paragraph;
 
-  //       return (
-  //         <div className={classes.image}>
-  //           <Image
-  //             src={`/images/posts/${post.slug}/${image.url}`}
-  //             alt={image.alt}
-  //             width={600}
-  //             height={300}
-  //           />
-  //         </div>
-  //       );
-  //     }
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
 
-  //     return <p>{paragraph.children}</p>;
-  //   },
-  // };
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    // code(code) {
+    //   const { language, value } = code;
+    //   return (
+    //     <SyntaxHighlighter
+    //       style={atomDark}
+    //       language={language}
+    //       children={value}
+    //     />
+    //   );
+    // },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customComponents}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 }
