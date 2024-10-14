067495d67f6724de8e935104044f9c9c52e7dfa0
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-10-14 19:05:21 +0300

    work

---
+++ UDEMY FRONT END COURSE/Tailwind CSS/section-4/3-webpack-tailwind-starter/src/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+};
