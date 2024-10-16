3f217fcf03ba0445a9c4af5c568dd54d9e231ce4
Mehmet Deniz Gucdemir <99511022+mdgucdemir@users.noreply.github.com>
2024-10-17 01:53:47 +0300

    .

--- UDEMY FRONT END COURSE/Tailwind CSS/secton-9-project-4/tailwind.config.js
+++ UDEMY FRONT END COURSE/Tailwind CSS/secton-9-project-4/tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   screens: {
-    sm: '480px',
-    md: '768px',
-    lg: '976px',
-    xl: '1440px',
+    sm: "480px",
+    md: "768px",
+    lg: "976px",
+    xl: "1440px",
   },
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Barlow Semi Condensed', 'sans-serif'],
+        sans: ["Barlow Semi Condensed", "sans-serif"],
       },
     },
   },
-  plugins: [require('@tailwindcss/line-clamp')],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
